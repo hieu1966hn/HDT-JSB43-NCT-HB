61fddde1a938de5a628491040c0f688b76e88c3b
hieu1966hn <hieu1966hn@gmail.com>
2023-03-18 16:05:53 +0700

    Done code SS6

--- SS6/app.js
+++ SS6/app.js
@@ -1,1 +1,19 @@
 // Đây là nơi code ngôn ngữ JS
+/*
+Kiểu dữ liệu trong JS: 
+1. Number: 1,2,4, 4.5, 5.6, ... 
+2. Boolean: true/false
+3. String: 'Đây là chuỗi 1', "Đây là chuỗi 2", `Đây là chuỗi 3`
+4. Null: không có giá trị nào cả
+5. Undefinded: không xác định (...)
+6. Object:  Thể hiện đối tượng có các thuộc tính truy cập đến
+
+
+Lưu trữ dữ liệu trong JS vào đâu?
+- Biến: vùng chứa dữ liệu trong JS
+- Cú pháp khai báo: let <ten bien>;
+*/
+
+let text1 = "Đây là biến Text";
+text1 = `Đây là biến đã được thay đổi lại nội dung`;
+text1 = 123;
